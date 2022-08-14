a12809d13ce2d627e3099a98e7791f2f5059b9e4
Mary Frye <deimospho@gmail.com>
2022-08-14 12:15:41 -0400

    ADDED gallery ref to app.js, ADDED first components to gallery.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 // Import data
-
+import React from 'react';
 // Import components
 import './App.css';
 
@@ -8,7 +8,7 @@ function App(){
     <div>
       <h1>craigslist</h1>
       <div className="App">
-        {/* Your content will go here! */}
+        {<Gallery />}
       </div>
     </div>
   );
